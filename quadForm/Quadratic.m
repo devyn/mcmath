17ef898d99f6be6492d7c813fe302877fc398720
devyn.cairns@gmail.com <devyn.cairns@gmail.com@e53f4f17-415e-1699-44e4-b8ee56396658>
2011-02-04 19:57:34 +0000

    placeholder code for Quadratic
    git-svn-id: https://mcmathdevyncairns.googlecode.com/svn/trunk@11 e53f4f17-415e-1699-44e4-b8ee56396658

--- quadForm/Quadratic.m
+++ quadForm/Quadratic.m
@@ -8,11 +8,22 @@
 
 #import "Quadratic.h"
 
-
 @implementation Quadratic
 
 - (IBAction)calculate:(id)sender
 {
+	double a, b, c, answerX1, answerX2;
+	
+	a = [fieldA doubleValue];
+	b = [fieldB doubleValue];
+	c = [fieldC doubleValue];
+	
+	/* placeholder code */
+	answerX1 = a+b+c;
+	answerX2 = answerX1;
+	
+	[fieldX1 setDoubleValue:answerX1];
+	[fieldX2 setDoubleValue:answerX2];
 }
 
 @end
