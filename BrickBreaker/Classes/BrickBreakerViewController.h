cf8b5ab75d65f6db4fd3eb15d1d5120370c86345
devyn.cairns@gmail.com <devyn.cairns@gmail.com@e53f4f17-415e-1699-44e4-b8ee56396658>
2011-02-15 20:57:21 +0000

    BrickBreaker part 2. Some typos in the instructions needed to be corrected.
    git-svn-id: https://mcmathdevyncairns.googlecode.com/svn/trunk@18 e53f4f17-415e-1699-44e4-b8ee56396658

--- BrickBreaker/Classes/BrickBreakerViewController.h
+++ BrickBreaker/Classes/BrickBreakerViewController.h
@@ -11,13 +11,17 @@
 @interface BrickBreakerViewController : UIViewController {
 	UILabel *scoreLabel;
 	UIImageView *ball;
+	UIImageView *paddle;
+	
 	int score;
 	
 	CGPoint ballMovement;
+	float touchOffset;
 }
 
 @property (nonatomic, retain) IBOutlet UILabel *scoreLabel;
 @property (nonatomic, retain) IBOutlet UIImageView *ball;
+@property (nonatomic, retain) IBOutlet UIImageView *paddle;
 
 - (void)initializeTimer;
 - (void)animateBall:(NSTimer *)theTimer;
--- BrickBreaker/Classes/BrickBreakerViewController.m
+++ BrickBreaker/Classes/BrickBreakerViewController.m
@@ -11,6 +11,7 @@
 @implementation BrickBreakerViewController
 @synthesize scoreLabel;
 @synthesize ball;
+@synthesize paddle;
 
 
 /*
@@ -71,6 +72,39 @@
 {
 	ball.center = CGPointMake(ball.center.x+ballMovement.x, ball.center.y+ballMovement.y);
 	
+	BOOL paddleCollision =
+		 ball.center.y >= paddle.center.y - 16 &&
+		 ball.center.y <= paddle.center.y + 16 &&
+		 ball.center.x >  paddle.center.x - 32 &&
+		 ball.center.x <  paddle.center.x + 32;
+	
+	//NSLog(@"paddle collision? %d", (int)paddleCollision);
+	
+	if (paddleCollision)
+	{
+		ballMovement.y = -ballMovement.y; // reverses the direction
+		if (ball.center.y >= paddle.center.y - 16
+			&& ballMovement.y < 0)
+		{
+			ball.center = CGPointMake(ball.center.x, paddle.center.y - 16);
+		}
+		else if (ball.center.y <= paddle.center.y + 16
+				 && ballMovement.y > 0)
+		{
+			ball.center = CGPointMake(ball.center.x, paddle.center.y + 16);
+		}
+		else if (ball.center.x >= paddle.center.x - 32
+				 && ballMovement.x < 0)
+		{
+			ball.center = CGPointMake(ball.center.x - 32, paddle.center.y);
+		}
+		else if (ball.center.x <= paddle.center.x + 32
+				 && ballMovement.x > 0)
+		{
+			ball.center = CGPointMake(ball.center.x + 32, paddle.center.y);
+		}
+	}
+	
 	if (ball.center.x > 300 || ball.center.x < 20)
 		ballMovement.x = -ballMovement.x;
 	
@@ -78,9 +112,35 @@
 		ballMovement.y = -ballMovement.y;
 }
 
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+	UITouch *touch = [[event allTouches] anyObject];
+	touchOffset = paddle.center.x - [touch locationInView:touch.view].x;
+}
+
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
+{
+	UITouch *touch = [[event allTouches] anyObject];
+	
+	float distanceMoved =
+		([touch locationInView:touch.view].x
+		   + touchOffset) - paddle.center.x;
+	
+	float newX = paddle.center.x + distanceMoved;
+	
+	if (newX > 30 && newX < 290)
+		paddle.center = CGPointMake(newX, paddle.center.y);
+	if (newX > 290)
+		paddle.center = CGPointMake(290, paddle.center.y);
+	if (newX < 30)
+		paddle.center = CGPointMake(30, paddle.center.y);
+	
+}
+
 - (void)dealloc {
 	[scoreLabel release];
 	[ball release];
+	[paddle release];
     [super dealloc];
 }
 
